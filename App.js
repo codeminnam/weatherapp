e6a7f366de5ec6296d880783d389107dbfb0b41e
cineminnam <cine.minnam@gmail.com>
2020-01-23 22:49:09 +0900

    loading page added

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello party people!</Text>
-    </View>
+    <Loading></Loading>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 2,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color: "orange"
-  }
-});
